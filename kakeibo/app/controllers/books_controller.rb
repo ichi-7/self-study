2dd3765d23831c99cc994be8a3de3ad54dc16438
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-09-29 07:56:06 +0000

    [Finish] sankousho

--- kakeibo/app/controllers/books_controller.rb
+++ kakeibo/app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
   
+  before_action :set_book, only:[:show,:edit,:update,:destroy]
+  
   def index
     @books = Book.all
+    @books = @books.where(year: params[:year]) if params[:year].present?
+    @books = @books.where(month: params[:month]) if params[:month].present?
   end
   
   def show
-    @book = Book.find(params[:id])
   end
   
   def new
@@ -13,14 +16,43 @@ class BooksController < ApplicationController
   end
   
   def create
-    book_params =  params.require(:book).permit(:year,:month,:inout,:category,:amount)
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "家計簿に#{@book[:year]}年#{@book[:month]}月の#{@book[:category]}を登録しました"
       redirect_to books_path
     else
+      flash.now[:alert] = "登録に失敗しました"
       render :new
-      flash[:notice] = "error"
     end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @book.update(book_params)
+      flash[:notice] = "データを1件登録しました"
+      redirect_to book_path(@book)
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book.destroy
+    flash[:notice] = "削除しました"
+    redirect_to books_path
+  end
+  
+  private
+  
+  def set_book
+    @book = Book.find(params[:id])
+  end
+  
+  def book_params
+    book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
   end
   
 end
--- kakeibo/config/routes.rb
+++ kakeibo/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   
-  get "/books", to: "books#index"
-  post "/books", to: "books#create"
-  get "/books/new", to: "books#new", as: "new_book"
-  get "/books/:id", to: "books#show", as: "book"
+  resources :books
   
 end
