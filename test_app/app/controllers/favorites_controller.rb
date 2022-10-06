b3a74f46bae2f9f33afd8558271a975d9b00950b
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-10-06 01:55:58 +0000

    save

--- test_app/app/controllers/favorites_controller.rb
+++ test_app/app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
   def create
     favorite = Favorite.new(user_id: current_user.id, spot_id: params[:spot_id])
     favorite.save
-    redirect_to spot_path(params[:spot_id])
+    
   end
   
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, spot_id: params[:spot_id])
     favorite.destroy
-    redirect_to spot_path(params[:spot_id])
+    
   end
   
 end
